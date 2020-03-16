73645af42a28b1dad001cf58b5b58866facd9505
Yamamotonasu <deep.blue.deep@outlook.jp>
2020-03-16 20:54:22 +0900

    [Create] Extend UIViewController

---
+++ fetilip/src/Extensions/UIViewController+Extended.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Extended.swift
+//  fetilip
+//
+//  Created by 山本裕太 on 2020/03/16.
+//  Copyright © 2020 YutaYamamoto. All rights reserved.
+//
+
+import Foundation
+import NSObject_Rx
+import UIKit
+
+extension UIViewController {
+
+}
