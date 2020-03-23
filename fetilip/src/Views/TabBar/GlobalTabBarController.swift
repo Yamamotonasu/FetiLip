0fa7cd6adcec783c174c205d5a0c895cdcc3755d
Yamamotonasu <deep.blue.deep@outlook.jp>
2020-03-23 20:54:47 +0900

    [Add] TabBar MyPage PostList ・仮のマイページ、一覧ページを作成 ・TabBarも仮作成

---
+++ fetilip/src/Views/TabBar/GlobalTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  GlobalTabBarController.swift
+//  fetilip
+//
+//  Created by 山本裕太 on 2020/03/23.
+//  Copyright © 2020 YutaYamamoto. All rights reserved.
+//
+
+import UIKit
+import PTCardTabBar
+
+class GlobalTabBarController: PTCardTabBar {
+
+}
