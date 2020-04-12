3d6132c2a06214ae82a2e7ff66d35c320752f8e8
Yamamotonasu <deep.blue.deep@outlook.jp>
2020-04-12 18:39:54 +0900

    wip

---
+++ fetilip/src/Firestore/Auth/UsersModel.swift
@@ -0,0 +1,21 @@
+//
+//  UsersAuthModel.swift
+//  fetilip
+//
+//  Created by 山本裕太 on 2020/04/06.
+//  Copyright © 2020 YutaYamamoto. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import FirebaseAuth
+
+/**
+ * Firebase
+ */
+public struct UsersAuthModel {
+
+    init() {}
+
+
+}
