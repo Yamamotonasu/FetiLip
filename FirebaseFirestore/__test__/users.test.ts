51ef562905d9e0d9c7e69e7c1e3794f1fd584013
Yamamotonasu <deep.blue.deep@outlook.jp>
2020-09-08 14:26:41 +0900

    yarn setup

---
+++ FirebaseFirestore/__test__/users.test.ts
@@ -0,0 +1,49 @@
+import * as firebase from "@firebase/testing";
+import * as fs from "fs";
+import { create } from "domain";
+
+const PROJECT_ID = "fetilip";
+const RULES_PATH = "firestore.rules";
+
+// 認証付きのFreistore appを作成する
+const createAuthApp = (auth?: object): firebase.firestore.Firestore => {
+  return firebase
+    .initializeTestApp({ projectId: PROJECT_ID, auth: auth })
+    .firestore();
+};
+
+// 管理者権限で操作できるFreistore appを作成する
+const createAdminApp = (): firebase.firestore.Firestore => {
+  return firebase.initializeAdminApp({ projectId: PROJECT_ID }).firestore();
+};
+
+// user情報への参照を作る
+const usersRef = (db: firebase.firestore.Firestore) => db.collection("/version/1/users");
+
+describe("Firestoreセキュリティルール", () => {
+  // ルールファイルの読み込み
+  beforeAll(async () => {
+    await firebase.loadFirestoreRules({
+      projectId: PROJECT_ID,
+      rules: fs.readFileSync(RULES_PATH, "utf8")
+    });
+  });
+
+  // Firestoreデータのクリーンアップ
+  afterEach(async () => {
+    await firebase.clearFirestoreData({ projectId: PROJECT_ID });
+  });
+
+  // Firestoreアプリの削除
+  afterAll(async () => {
+    await Promise.all(firebase.apps().map(app => app.delete()));
+  });
+
+  // 以降にテストを記載
+  
+  test("Firebase test", async () => {
+    const db = createAuthApp();
+    const user = usersRef(db).doc("test");
+    await firebase.assertSucceeds(user.get()); 
+  });
+});
