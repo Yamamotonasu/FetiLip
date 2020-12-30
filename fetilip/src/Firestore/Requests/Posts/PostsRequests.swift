eec6483501b465164ab3f9134a8f8850a4bfd21a
Yamamotonasu <deep.blue.deep@outlook.jp>
2020-12-30 12:23:01 +0900

    Add userUid field in posts document

--- fetilip/src/Firestore/Requests/Posts/PostsRequests.swift
+++ fetilip/src/Firestore/Requests/Posts/PostsRequests.swift
@@ -14,14 +14,15 @@ public enum PostsRequests: FirestoreRequest {
 
     public typealias Fields = PostModel.FieldType
 
-    case postImage(review: String, userRef: DocumentReference, imageRef: String)
+    case postImage(review: String, userRef: DocumentReference, userUid: String, imageRef: String)
 
     public var parameters: Parameters {
         var params: Parameters = [:]
         switch self {
-        case .postImage(let review, let userRef, let imageRef):
+        case .postImage(let review, let userRef, let userUid, let imageRef):
             params[Fields.Key.review.rawValue] = review
             params[Fields.Key.userRef.rawValue] = userRef
+            params[Fields.Key.userUid.rawValue] = userUid
             params[Fields.Key.imageRef.rawValue] = imageRef
             params[Fields.Key.createdAt.rawValue] = FieldValue.serverTimestamp()
             params[Fields.Key.updatedAt.rawValue] = FieldValue.serverTimestamp()
