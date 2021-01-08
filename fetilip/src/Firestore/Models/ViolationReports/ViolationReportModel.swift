f97abe8058c0d8f948ba9ec8602036484b9e8ea6
Yamamotonasu <deep.blue.deep@outlook.jp>
2021-01-08 10:11:49 +0900

    Add violation report model

---
+++ fetilip/src/Firestore/Models/ViolationReports/ViolationReportModel.swift
@@ -0,0 +1,26 @@
+//
+//  ViolationReportModel.swift
+//  fetilip
+//
+//  Created by yuu yamamoto on 2021/01/08.
+//  Copyright © 2021 YutaYamamoto. All rights reserved.
+//
+
+import Foundation
+
+public struct ViolationReportModel: FirestoreDatabaseCollection {
+
+    public typealias FieldType = ViolationReportEntity
+
+    public static let collectionName: String = "violationRepots"
+
+    public let id: String
+
+    public let fields: FieldType?
+
+    public init(id: String, fields: FieldType?) {
+        self.id = id
+        self.fields = fields
+    }
+
+}
