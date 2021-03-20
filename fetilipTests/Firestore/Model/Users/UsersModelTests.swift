cfedbbcd4cd7cf70b9a7fca93d7838eb34adcc01
Yamamotonasu <deep.blue.deep@outlook.jp>
2021-03-20 11:59:48 +0900

    compatible Xcode12.5

--- fetilipTests/Firestore/Model/Users/UsersModelTests.swift
+++ fetilipTests/Firestore/Model/Users/UsersModelTests.swift
@@ -1,19 +1,19 @@
+////
+////  UsersModelTests.swift
+////  fetilipTests
+////
+////  Created by 山本裕太 on 2020/06/20.
+////  Copyright © 2020 YutaYamamoto. All rights reserved.
+////
 //
-//  UsersModelTests.swift
-//  fetilipTests
+//import XCTest
+//@testable import fetilip
+//import Quick
+//import Nimble
 //
-//  Created by 山本裕太 on 2020/06/20.
-//  Copyright © 2020 YutaYamamoto. All rights reserved.
+//class TestUserModel: QuickSpec, LoginFunction {
+//    var selfUid: String?
+//}
 //
-
-import XCTest
-@testable import fetilip
-import Quick
-import Nimble
-
-class TestUserModel: QuickSpec, LoginFunction {
-    var selfUid: String?
-}
-
-
-
+//
+//
